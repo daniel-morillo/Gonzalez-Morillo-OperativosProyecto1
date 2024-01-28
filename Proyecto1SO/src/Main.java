c472bd88891597aba2dd6ad92b2810fc6cc4fbad
GAUGUSTO1602 <97412793+GAUGUSTO1602@users.noreply.github.com>
2024-01-27 20:39:59 -0400

    Conexion del drive entre company y assembler

--- Proyecto1SO/src/Main.java
+++ Proyecto1SO/src/Main.java
@@ -19,23 +19,17 @@ public class Main {
         // TODO code application logic here
         
         
-        //Realmente aqui solo se arrancan las interfaces, sin embargo esto es para probar. 
-        Drive drive = new Drive(10,10,10,10,10);
+         
         Semaphore mutex = new Semaphore(1);
         
-        Developer guionistas = new Developer(3,0,20,2000,mutex,2,1,drive);
-        Developer animadores = new Developer(1,2,40,2000,mutex,1,1,drive);
-        Developer dobladores = new Developer(2,3,10,2000,mutex,3,1,drive);
-        Developer escenarios = new Developer(2,1,35,2000,mutex,1,1,drive);
-        Developer plotTwist = new Developer(2,4,34,2000,mutex,3,1,drive);
+        //Realmente aqui solo se arrancan las interfaces, sin embargo esto es para probar.
+        Company CN = new Company(2, 2, 2, 2, 2, 10, 20, 30, 20, 40, 3000, 1, 2, 3, 2, 1, 3, 2, 4, 5, 10);
+        Drive drive = CN.getDrive();
         Assembler ensamblador = new Assembler(1, 50, 2000, mutex, 2, drive, 1, 2, 6, 5, 1, 3);
         
+        
+        CN.StartWorking();
         ensamblador.start();
-        guionistas.start();
-        animadores.start();
-        dobladores.start();
-        escenarios.start();
-        plotTwist.start();
         
     }
     
